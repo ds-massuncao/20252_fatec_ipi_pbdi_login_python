3abec3beea62e0aba10b567e0817e249305e788c
Marcos Assunção da Silva <marcos.silva455@fatec.sp.gov.br>
2025-10-05 16:35:17 -0300

    feat(ap07-plsql): implementa conexao com banco

--- login.py
+++ login.py
@@ -5,3 +5,25 @@ class Usuario:
     def __init__(self, login, senha):
         self.login = login
         self.senha = senha
+
+def existe (usuario):
+#Abre a conexão
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="20252_fatec_ipi_pbdi_login_python",
+        user="postgres",
+        password="pLub3n4TTi@#$"
+    ) as conexao:
+        #obtém um cursor
+        with conexao.cursor() as cursor:
+                #executa o comando
+            cursor.execute('SELECT * FROM tb_usuario WHERE login=%s AND senha=%s', (f'{usuario.login}',
+            f'{usuario.senha}'))
+            #obtém o resultado
+            result = cursor.fetchone()
+            #verifica se o resultado é diferente de None, o
+            #que indica que o usuário existe na base
+    return result != None
+
+            
