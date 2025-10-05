93a15640d425c094c807f81e56e0f2ddc3dba001
Marcos Assunção da Silva <marcos.silva455@fatec.sp.gov.br>
2025-10-05 17:24:31 -0300

    refactor(ap07-plsql): implementa insere usuario

--- login.py
+++ login.py
@@ -13,7 +13,7 @@ def existe (usuario):
         port=5432,
         dbname="20252_fatec_ipi_pbdi_login_python",
         user="postgres",
-        password="pLub3n4TTi@#$"
+        password="Lub3n4TTi@#$"
     ) as conexao:
         #obtém um cursor
         with conexao.cursor() as cursor:
@@ -26,10 +26,27 @@ def existe (usuario):
             #que indica que o usuário existe na base
     return result != None
 
+def registar(usuario):
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="20252_fatec_ipi_pbdi_login_python",
+        user="postgres",
+        password="Lub3n4TTi@#$"
+    ) as conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute(
+                'INSERT INTO tb_usuario (login, senha) VALUES (%s, %s)',
+                (usuario.login, usuario.senha)
+            )
+        conexao.commit()
+        conexao.close()
+    return True
+            
             
 def menu():
     
-    texto = '0 - Fechar Sistema\n1 - Login\n2 - Logoff\n'
+    texto = '0 - Fechar Sistema\n1 - Login\n2 - Logoff\n3 - Inserir Usuario\n'
     
     usuario = None
     
@@ -46,7 +63,15 @@ def menu():
         if op == 2:
             usuario = None
             print("Logoff efetuado com sucesso!!!")
-        op == int(input(texto))
+        
+        
+        if op == 3:
+            login = input('Login: ')
+            senha = input('Senha: ')
+            usuario = Usuario(login, senha)
+            print("Usuario Inserido" if registar(usuario) else "Erro ao inserir usuario")
+            
+        op = int(input(texto))
         
     else:
         print("Até mais!!!")      
